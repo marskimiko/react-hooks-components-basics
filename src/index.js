2f914a413f85ada6a988ca498c4be9edde52eae8
marisasaunders <marisaksaunders@gmail.com>
2022-03-09 16:15:54 -0500

    commit tree 953b0b32ac57fbcdaa8b8ac0c2bfbf195d7459ab

--- src/index.js
+++ src/index.js
@@ -2,4 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+function Article() {
+  return (
+    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+  );
+}
+function Comment() {
+  return <div>Naturally, I agree with this article.</div>;
+}
+
+function App() {
+  return (
+    <div>
+      <Article />
+      <Comment />
+    </div>
+  );
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
